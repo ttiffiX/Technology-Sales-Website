d619b07b07a40e8f14e02a0642d4b9461a615795
Sang Phạm <sangpham1224@gmail.com>
2026-01-03 00:10:11 +0700

    feat(verify email): add check email exist (fe)

--- FE/my-react-app/src/pages/verifyemail/WaitingVerification.js
+++ FE/my-react-app/src/pages/verifyemail/WaitingVerification.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {useNavigate, useLocation} from 'react-router-dom';
 import './WaitingVerification.scss';
 
 const WaitingVerification = () => {
@@ -25,8 +25,10 @@ const WaitingVerification = () => {
                 <div className="waiting-content">
                     <div className="email-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                            <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
-                            <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
+                            <path
+                                d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z"/>
+                            <path
+                                d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z"/>
                         </svg>
                     </div>
 
@@ -61,12 +63,6 @@ const WaitingVerification = () => {
                             </div>
                         </div>
                     </div>
-
-                    {resendMessage && (
-                        <div className="status-message">
-                            {resendMessage}
-                        </div>
-                    )}
 
                     <div className="action-buttons">
                         <button
